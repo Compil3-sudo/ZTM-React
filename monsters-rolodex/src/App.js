8e998b50360837db0c190c36e576e6a5dd497911
tincurar <tincurares13@gmail.com>
2023-07-25 00:10:19 +0300

    work in progress

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+
+    // this.state = {
+    //   name: { firstName: "Some", lastName: "Body" },
+    //   course: "ZTM-React",
+    // };
+
+    // this.state = {
+    //   monsters: [
+    //     {
+    //       name: "Linda",
+    //       id: Math.random(),
+    //     },
+    //     {
+    //       name: "Frank",
+    //       id: Math.random(),
+    //     },
+    //     {
+    //       name: "Jacky",
+    //       id: Math.random(),
+    //     },
+    //   ],
+    // };
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    // make API request
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "2rem" }}>
+        {/* <div>App Component</div>
+        <h1>
+          {this.state.name.firstName} {this.state.name.lastName}
+        </h1>
+        <h2>Learning: {this.state.course}</h2>
+        <button
+          onClick={() => {
+            this.setState(
+              () => {
+                return {
+                  name: { firstName: "New", lastName: "Name" },
+                };
+              },
+              // callBack function
+              () => {
+                console.log(this.state);
+              }
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Change Name
+        </button> */}
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
