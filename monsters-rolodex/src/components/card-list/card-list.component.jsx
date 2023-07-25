9509df8e82737a157ad4e772a4784adaddda945a
tincurar <tincurares13@gmail.com>
2023-07-25 14:31:05 +0300

    finished function components

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -2,16 +2,30 @@ import React, { Component } from "react";
 import "./card-list.styles.css";
 import Card from "../card/card.component";
 
-export default class CardList extends Component {
-  render() {
-    const { filteredMonsters } = this.props;
+const CardList = (props) => {
+  const { filteredMonsters } = props;
 
-    return (
-      <div className="card-list">
-        {filteredMonsters.map((monster) => {
-          return <Card key={monster.id} monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-list">
+      {filteredMonsters.map((monster) => {
+        return <Card key={monster.id} monster={monster} />;
+      })}
+    </div>
+  );
+};
+
+export default CardList;
+
+// export default class CardList extends Component {
+//   render() {
+//     const { filteredMonsters } = this.props;
+
+//     return (
+//       <div className="card-list">
+//         {filteredMonsters.map((monster) => {
+//           return <Card key={monster.id} monster={monster} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
--- monsters-rolodex/src/components/search-box/search-box.component.jsx
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import "./search-box.styles.css";
 
-export default class SearchBox extends Component {
-  render() {
-    const { onSearchChange } = this.props;
-    return (
-      <input
-        className={`search-box ${this.props.className}}`}
-        type="search"
-        placeholder={this.props.placeholder}
-        onChange={onSearchChange}
-      />
-    );
-  }
-}
+const SearchBox = (props) => {
+  const { onSearchChange } = props;
+
+  return (
+    <input
+      className={`search-box ${props.className}}`}
+      type="search"
+      placeholder={props.placeholder}
+      onChange={onSearchChange}
+    />
+  );
+};
+
+export default SearchBox;
+
+// export default class SearchBox extends Component {
+//   render() {
+//     const { onSearchChange } = this.props;
+//     return (
+//       <input
+//         className={`search-box ${this.props.className}}`}
+//         type="search"
+//         placeholder={this.props.placeholder}
+//         onChange={onSearchChange}
+//       />
+//     );
+//   }
+// }
