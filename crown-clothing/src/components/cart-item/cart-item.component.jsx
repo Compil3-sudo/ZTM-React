b690c320cfa18a661390c328c08addf836307131
tincurar <tincurares13@gmail.com>
2023-07-26 19:45:46 +0300

    added to cart

---
+++ crown-clothing/src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./cart-item.styles.scss";
+
+const CartItem = ({ cartItem }) => {
+  return (
+    <div className="cart-item-container">
+      <img src={cartItem.imageUrl} alt={cartItem.name} />
+      <div className="item-details">
+        <span className="name">{cartItem.name}</span>
+        <span className="price">
+          {cartItem.quantity} x ${cartItem.price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- crown-clothing/src/components/product-card/product-card.component.jsx
+++ crown-clothing/src/components/product-card/product-card.component.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "../button/button.component";
 import "./product-card.styles.scss";
+import { CartContext } from "../../contexts/cart-context.component";
 
 const ProductCard = ({ product }) => {
+  const cartCtx = useContext(CartContext);
+
+  const addProductToCart = () => {
+    cartCtx.addItemToCart(product);
+  };
+
   return (
     <div className="product-card-container">
       <img src={product.imageUrl} alt={product.name} />
+
       <div className="footer">
         <span className="name">{product.name}</span>
         <span className="price">$ {product.price}</span>
       </div>
-      <Button buttonType="inverted">Add to cart</Button>
+
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to cart
+      </Button>
     </div>
   );
 };
