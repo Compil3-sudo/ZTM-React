270f5bc3fad980a013c1a8c8fc1c5bfb301e2ad8
tincurar <tincurares13@gmail.com>
2023-07-27 15:28:01 +0300

    worked on buttons

--- crown-clothing/src/components/button/button.component.jsx
+++ crown-clothing/src/components/button/button.component.jsx
@@ -11,12 +11,13 @@ export const BUTTON_TYPE_CLASSES = {
   inverted: "inverted",
 };
 
-const getButtonType = (buttonType = BUTTON_TYPE_CLASSES.base) =>
-  ({
+const getButtonType = (buttonType = BUTTON_TYPE_CLASSES.base) => {
+  return {
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
     [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-  }[buttonType]);
+  }[buttonType];
+};
 
 const Button = ({ children, buttonType, ...otherProps }) => {
   const CustomButton = getButtonType(buttonType);
--- crown-clothing/src/components/product-card/product-card.component.jsx
+++ crown-clothing/src/components/product-card/product-card.component.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import "./product-card.styles.scss";
 import { CartContext } from "../../contexts/cart-context.component";
 
@@ -19,7 +19,10 @@ const ProductCard = ({ product }) => {
         <span className="price">$ {product.price}</span>
       </div>
 
-      <Button buttonType="inverted" onClick={addProductToCart}>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+        onClick={addProductToCart}
+      >
         Add to cart
       </Button>
     </div>
