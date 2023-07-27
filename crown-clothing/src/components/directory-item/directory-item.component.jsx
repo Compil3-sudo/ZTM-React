5a78d2e671146b80fcecae8964cb84328fc30234
tincurar <tincurares13@gmail.com>
2023-07-27 14:38:38 +0300

    added directory routing

--- crown-clothing/src/components/directory-item/directory-item.component.jsx
+++ crown-clothing/src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import "./directory-item.styles.scss";
 import React from "react";
 
 const DirectoryItem = (props) => {
   const category = props.category;
 
+  const navigate = useNavigate();
+
+  const redirectToDirectory = () => {
+    navigate("/shop/" + category.title);
+  };
+
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={redirectToDirectory}>
       <div
         className="background-image"
         style={{
