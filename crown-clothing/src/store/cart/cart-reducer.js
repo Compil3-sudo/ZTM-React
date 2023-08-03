3ac87fe8200a122fb692c8f02b07ecaed7453ddb
tincurar <tincurares13@gmail.com>
2023-08-03 21:23:04 +0300

    removed unused code

--- crown-clothing/src/store/cart/cart-reducer.js
+++ crown-clothing/src/store/cart/cart-reducer.js
@@ -1,10 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import {
-//   CART_ACTION_TYPES,
-//   addCartItem,
-//   decrementCartItem,
-//   deleteCartItem,
-// } from "./cart-actions";
 
 export const CART_INITIAL_STATE = {
   isCartOpen: false,
@@ -90,31 +84,6 @@ export const cartSlice = createSlice({
     },
   },
 });
-
-// export const cartReducer = (state = CART_INITIAL_STATE, action) => {
-//   const { type, payload } = action;
-
-//   switch (type) {
-//     // returns new cart state
-//     case CART_ACTION_TYPES.ADD_ITEM_TO_CART:
-//       return addCartItem(state, payload);
-
-//     case CART_ACTION_TYPES.DECREMENT_CART_ITEM:
-//       return decrementCartItem(state, payload);
-
-//     case CART_ACTION_TYPES.DELETE_CART_ITEM:
-//       return deleteCartItem(state, payload);
-
-//     case CART_ACTION_TYPES.SET_IS_CART_OPEN:
-//       return {
-//         ...state,
-//         isCartOpen: payload,
-//       };
-
-//     default:
-//       return state;
-//   }
-// };
 
 export const {
   setIsCartOpen,
