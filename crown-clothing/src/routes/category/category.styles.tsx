bb3ea638f00b30cc9fd70157672a1a8b80893a6b
tincurar <tincurares13@gmail.com>
2023-09-05 13:46:19 +0300

    center categories

--- crown-clothing/src/routes/category/category.styles.tsx
+++ crown-clothing/src/routes/category/category.styles.tsx
@@ -13,6 +13,7 @@ export const CategoryContainer = styled.div`
   display: grid;
   gap: 3rem 2rem;
   justify-items: center;
+  align-items: center;
   grid-template-columns: 1fr 1fr 1fr 1fr;
 
   @media screen and (max-width: 1100px) {
